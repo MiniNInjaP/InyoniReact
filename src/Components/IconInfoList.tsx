b2161c5637c89a1a21a8430371d083451a323f25
Marnus PC <marnusvw7@gmail.com>
2023-06-21 11:23:11 +0200

    Implement Responsive Design in the Icon Info List

--- src/Components/IconInfoList.tsx
+++ src/Components/IconInfoList.tsx
@@ -1,29 +1,42 @@
-import { Button, HStack, Icon, Link, Text } from "@chakra-ui/react";
+import { Button, HStack, Icon, Link, Show, Text } from "@chakra-ui/react";
 import { FaFacebookF, FaPhone, FaEnvelope } from "react-icons/fa";
 
 const IconInfoList = () => (
-  <HStack fontSize="xs" marginLeft="20vw">
+  <HStack
+    paddingTop={1}
+    wrap="wrap"
+    spacing={1}
+    fontSize={{ base: 14, md: 12 }}
+    marginLeft={{ md: "10vw", lg: "20vw" }}
+    justifyContent={{ base: "center", md: "center", lg: "flex-start" }}
+  >
     <Icon as={FaPhone} color="green" />
-    <Text color="green">Tel: +27 13 712 2175</Text>
-    <Text>| </Text>
-    <Text>Fax: +27 13 712 4198</Text>
+    <Text color="green">Tel: +27 13 712 2175 |</Text>
+    <Text color="green"> Fax: +27 13 712 4198</Text>
     <Button
+      margin={0}
       paddingLeft={2}
+      style={{ height: "20px" }}
       variant="unstyled"
-      fontSize="xs"
+      fontSize={{ base: 14, md: 12 }}
       color="green"
+      textAlign={"center"}
       leftIcon={<Icon as={FaEnvelope} />}
     >
-      inyoniAfrica.co.za
+      info@inyoniafrica.co.za
     </Button>
-
-    <Link target="_blank" href="https://facebook.com/InyoniAfrica/">
-      <Button
-        fontSize="sm"
-        variant="unstyled"
-        leftIcon={<Icon as={FaFacebookF} color="green" />}
-      ></Button>
-    </Link>
+    <Show above="sm">
+      <Link target="_blank" href="https://facebook.com/InyoniAfrica/">
+        <Button
+          style={{ height: "20px" }}
+          as="a"
+          paddingLeft={2}
+          fontSize={{ base: 14, md: 14 }}
+          variant="unstyled"
+          leftIcon={<Icon as={FaFacebookF} color="green" />}
+        ></Button>
+      </Link>
+    </Show>
   </HStack>
 );
 
