9c39f9ffd6fb6fd25982044521bc45e1ef823b4c
Marnus PC <marnusvw7@gmail.com>
2023-06-26 08:55:37 +0200

    Implement Facebok Button for Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,51 +1,97 @@
-import { Box, Card, CardBody, Icon, FormControl, FormLabel, HStack, Heading, Input, Text, Textarea } from '@chakra-ui/react'
-import { footerText } from '../TextInput/TextContent'
-import { FaFacebookF } from "react-icons/fa";
+import {
+  Box,
+  Card,
+  CardBody,
+  FormControl,
+  FormLabel,
+  HStack,
+  Heading,
+  IconButton,
+  Input,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
+import { footerText } from "../TextInput/TextContent";
 
-import Button from './Button'
-import { Link } from 'react-router-dom';
+import Button from "./Button";
+import { Link } from "react-router-dom";
+import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <>
-      <Card 
+      <Card
+        borderRadius={0}
+        paddingX={{ base: 0, lg: 200 }}
+        color={"white"}
+        flexDir={{ base: "column", lg: "row" }}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        backgroundColor={"#17733A"}
+      >
+        <CardBody>
+          <Heading paddingBottom={5}>Bussiness Information</Heading>
+          <Heading fontWeight={400} fontSize={15}>
+            Inyoni Africa Irrigation Specialists (Pty) Ltd
+          </Heading>
+          <Text lineHeight={8}>Registration number 2004/033490/07</Text>
+          <Heading fontWeight={400} fontSize={15}>
+            Contact details & Web address
+          </Heading>
+          <Text lineHeight={8} whiteSpace={"pre-wrap"}>
+            {footerText}
+          </Text>
+        </CardBody>
+        <Box width={500}>
+          <FormControl color="black" marginY={7}>
+            <FormLabel fontSize={20} color={"white"}>
+              Get in Touch
+            </FormLabel>
+            <HStack>
+              <Input
                 borderRadius={0}
-                  paddingX={200}
-                  color={'white'} 
-                  flexDir={'row'}
-                  justifyContent={'space-between'}
-                  backgroundColor={"#17733A"}>
-                    
-               <CardBody paddingLeft={200}>
-                  <Heading paddingBottom={5}>Bussiness Information</Heading>
-                  <Heading fontWeight={400} fontSize={15}>Inyoni Africa Irrigation Specialists (Pty) Ltd</Heading>
-                  <Text lineHeight={8} >Registration number 2004/033490/07</Text>
-                  <Heading fontWeight={400} fontSize={15} >Contact details & Web address</Heading>
-                  <Text lineHeight={8} whiteSpace={'pre-wrap'}>{footerText}</Text>
-                  
-               </CardBody>
-               <Box  width={500}>
-                   <FormControl  color='black' marginY={7}>
-                      <FormLabel fontSize={20} color={'white'}>Get in Touch</FormLabel>
-                      <HStack>
-                          <Input borderRadius={0} bgColor={"#FFF"} type='text' placeholder='Name'></Input>
-                          <Input  borderRadius={0} bgColor={"#FFF"} type='email' placeholder='Email Address'></Input>
-                      </HStack>
-                      <Textarea  bgColor={"#FFF"} height={150} borderRadius={0} marginY={2}  placeholder='Message' />
-                      <HStack justifyContent={'flex-end'}>
-                          <Button variant='secondary'>Submit</Button>
-                      </HStack>
-                   </FormControl>
-               </Box>
-              </Card>
-              <HStack  borderRadius={0} bgColor={"rgba(0,0,0,0.8)"}>
-                <Text  marginLeft={400} paddingRight={5} color={'white'}>Copyrights Inyoni Africa (PTY)Ltd.</Text>
-                <Link to="https://facebook.com/InyoniAfrica/" target="_blank" >
-                    <Button variant='secondary'>FaceBook</Button>
-                </Link>
-              </HStack>
+                bgColor={"#FFF"}
+                type="text"
+                placeholder="Name"
+              ></Input>
+              <Input
+                borderRadius={0}
+                bgColor={"#FFF"}
+                type="email"
+                placeholder="Email Address"
+              ></Input>
+            </HStack>
+            <Textarea
+              bgColor={"#FFF"}
+              height={150}
+              borderRadius={0}
+              marginY={2}
+              placeholder="Message"
+            />
+            <HStack justifyContent={"flex-end"}>
+              <Button variant="secondary">Submit</Button>
+            </HStack>
+          </FormControl>
+        </Box>
+      </Card>
+      <HStack
+        paddingRight={200}
+        paddingLeft={210}
+        justifyContent={"space-between"}
+        borderRadius={0}
+        bgColor={"rgba(0,0,0,0.8)"}
+      >
+        <Text paddingRight={5} color={"white"}>
+          Copyrights Inyoni Africa (PTY)Ltd.
+        </Text>
+        <Link target="_blank" to={"https://www.facebook.com/InyoniAfrica/"}>
+          <IconButton
+            aria-label="Facebook-Link"
+            icon={<FaFacebookF />}
+          ></IconButton>
+        </Link>
+      </HStack>
     </>
- 
   );
-}
-export default Footer
+};
+export default Footer;
